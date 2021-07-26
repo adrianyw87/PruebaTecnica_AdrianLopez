4ae58c914cec1fbcfd8d09f9b3a702538de4bbfe
ComparteHuerto <adrianyellowworm@gmail.com>
2021-07-26 19:28:57 +0200

    subida del proyecto

---
+++ New Unity Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance { get { return _instance; } }
+
+    private string screenToLoad;
+
+    public string levelName_Init;
+    public string levelName_LoadScreen;
+    public string levelName_Menu;
+    public string levelName_InGame;
+
+    public int counter;
+
+
+    private void Awake()
+    {
+        // Singleton pattern
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        // Don´t destroy on load
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+        // Set the value for the scenes to load
+        screenToLoad = levelName_LoadScreen;
+        GoToLoadScreen();
+        screenToLoad = levelName_Menu;
+    }
+
+    public void FixedUpdate()
+    {
+        counter++;
+    }
+
+    public void LoadScreen()
+    {
+        // Manage scenes
+        if (screenToLoad == levelName_LoadScreen)
+        {
+            GoToLoadScreen();
+        }
+        if (screenToLoad == levelName_Menu)
+        {
+            GotoMenu();
+        }
+    }
+
+    public void GoToLoadScreen()
+    {
+        SceneManager.LoadScene(levelName_LoadScreen);
+    }
+
+
+    public void GotoInit()
+    {
+        SceneManager.LoadScene(levelName_Init);
+    }
+
+    public void GotoMenu()
+    {
+        SceneManager.LoadScene(levelName_Menu);
+    }
+
+    public void GotoInGame()
+    {
+        SceneManager.LoadScene(levelName_InGame);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private GameObject flowManager;
+
+    void Start()
+    {
+        flowManager = GameObject.Find("FlowManager");
+        StartCoroutine(AsyncLoad());
+    }
+
+    void Update()
+    {
+        Debug.Log("loading..." + flowManager.GetComponent<GameManager>().counter);
+    }
+
+    public IEnumerator AsyncLoad()
+    {
+        yield return new WaitForSeconds(3);
+        flowManager.GetComponent<GameManager>().LoadScreen();
+    }
+}
